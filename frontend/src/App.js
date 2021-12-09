bc417f602852de3337f034f5ca8f5502e93a7608
Milan Radojevic <mikister2012@gmail.com>
2021-12-09 19:37:44 +0100

    frontend: Add basic layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,36 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="app">
+      <h1 className="title">Shopify example app</h1>
+
+      <main>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+        <p>Content</p>
+      </main>
+
+      <nav className="navigation">
+        <form>
+          <ul>
+            <li>
+              <button>← Nazaj</button>
+            </li>
+            <li>
+              <button>1</button>
+            </li>
+            <li>
+              <button>2</button>
+            </li>
+            <li>
+              <button>3</button>
+            </li>
+            <button>Naprej →</button>
+          </ul>
+        </form>
+      </nav>
     </div>
   );
 }
